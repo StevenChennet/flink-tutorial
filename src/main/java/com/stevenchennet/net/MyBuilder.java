4616c05c1f7790b2bbd07c7cf4865af8427c6517
ChenRudai <StevenChennet@live.com>
2020-01-17 12:15:45 +0800

    add FlinkApp

---
+++ src/main/java/com/stevenchennet/net/MyBuilder.java
@@ -0,0 +1,16 @@
+package com.stevenchennet.net;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class MyBuilder {
+//    private final Supplier<T> instantiator;
+//    private List<Consumer<T>> modifiers = new ArrayList<>();
+//    public MyBuilder(Supplier<T> instantiator){
+//        this.instantiator = instantiator;
+//    }
+
+
+
+}

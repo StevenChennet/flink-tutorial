492b8be258ea6a18ebc0c5e64da9e40f8576413f
ChenRudai <StevenChennet@live.com>
2020-01-18 15:15:33 +0800

    add FlinkApp

---
+++ src/main/java/com/stevenchennet/net/builder/BuildEntityApp.java
@@ -0,0 +1,44 @@
+package com.stevenchennet.net.builder;
+
+public class BuildEntityApp {
+    static void Test() {
+        Entity entity = MyBuilder.of(Entity::new)
+                .with(Entity::setId, 1)
+                .with(Entity::setName, "zhangsan")
+                .with(Entity::setAbc, 12L)
+                .build();
+    }
+
+    static class Entity {
+        private int id;
+        private String name;
+        private long abc;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public long getAbc() {
+            return abc;
+        }
+
+        public void setAbc(long abc) {
+            this.abc = abc;
+        }
+
+
+    }
+
+}
---
+++ src/main/java/com/stevenchennet/net/builder/FIExampleApp.java
@@ -0,0 +1,46 @@
+package com.stevenchennet.net.builder;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class FIExampleApp {
+    public static void main(String[] args) {
+        //FuncEx();
+
+        ConsEx();;
+
+    }
+
+    private static void FuncEx() {
+        Function<Integer, Long> f1 = i -> {
+            System.out.println("f1 called.");
+            return i * 2L;
+        };
+        Function<Long, String> f2 = i -> {
+            System.out.println("f2 called.");
+            return "s" + i;};
+
+        Function<Integer, String> f3 = f1.andThen(f2);
+        System.out.println(f3.apply(2));
+
+        System.out.println("-----------------");
+
+        Function<Integer, String> f5 = f2.compose(f1);
+        System.out.println(f5.apply(3));
+    }
+
+    private static void ConsEx(){
+        Consumer<Integer> c1 = i->{
+            System.out.println("c1 called");
+        };
+
+        Consumer<Integer> c2 = i->{
+            System.out.println("c2 called");
+        };
+
+        Consumer<Integer> c3 = c1.andThen(c2);
+        c3.accept(4);
+    }
+
+
+}

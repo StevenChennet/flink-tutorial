c7a5cd41abcac79d1125eed87c93fa88ed9bd70b
StevenChennet <StevenChennet@live.com>
2020-01-31 00:55:56 +0800

    cogroup

--- src/main/java/com/stevenchennet/net/cogroup/KwhMeterSource.java
+++ src/main/java/com/stevenchennet/net/cogroup/KwhMeterSource.java
@@ -2,11 +2,31 @@ package com.stevenchennet.net.cogroup;
 
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class KwhMeterSource implements SourceFunction<KwhMeter> {
 
     @Override
     public void run(SourceContext<KwhMeter> ctx) throws Exception {
+        long TAG = 1000L;
 
+        List<KwhMeter> kwhMeterList = Arrays.asList(
+                KwhMeter.builder().id("A").billId("billId").uptTime(110 * TAG).kwhMeter(101).build(),
+                KwhMeter.builder().id("A").billId("billId").uptTime(140 * TAG).kwhMeter(102).build(),
+                KwhMeter.builder().id("A").billId("billId").uptTime(170 * TAG).kwhMeter(103).build(),
+                KwhMeter.builder().id("A").billId("billId").uptTime(200 * TAG).kwhMeter(104).build(),
+                KwhMeter.builder().id("A").billId("billId").uptTime(230 * TAG).kwhMeter(105).build(),
+                KwhMeter.builder().id("A").billId("billId").uptTime(260 * TAG).kwhMeter(106).build(),
+                KwhMeter.builder().id("A").billId("billId").uptTime(290 * TAG).kwhMeter(107).build(),
+                KwhMeter.builder().id("A").billId("billId").uptTime(320 * TAG).kwhMeter(108).build(),
+                KwhMeter.builder().id("A").billId("billId").uptTime(350 * TAG).kwhMeter(109).build(),
+                KwhMeter.builder().id("A").billId("billId").uptTime(380 * TAG).kwhMeter(110).build()
+        );
+
+        for (KwhMeter kwhMeter : kwhMeterList){
+            ctx.collectWithTimestamp(kwhMeter,kwhMeter.getUptTime());
+        }
     }
 
     @Override
--- src/main/java/com/stevenchennet/net/cogroup/PileDataSource.java
+++ src/main/java/com/stevenchennet/net/cogroup/PileDataSource.java
@@ -2,10 +2,29 @@ package com.stevenchennet.net.cogroup;
 
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PileDataSource implements SourceFunction<PileData> {
     @Override
     public void run(SourceContext<PileData> ctx) throws Exception {
+        long TAG = 1000;
+        List<PileData> pileDataList = Arrays.asList(
+                PileData.builder().id("A").billId("billId").uptTime(110 * TAG).DirectCurrent(30).DirectVoltage(380).DirectPower(10000).build(),
+                PileData.builder().id("A").billId("billId").uptTime(140 * TAG).DirectCurrent(30).DirectVoltage(380).DirectPower(10000).build(),
+                PileData.builder().id("A").billId("billId").uptTime(170 * TAG).DirectCurrent(30).DirectVoltage(380).DirectPower(10000).build(),
+                PileData.builder().id("A").billId("billId").uptTime(200 * TAG).DirectCurrent(30).DirectVoltage(380).DirectPower(10000).build(),
+                PileData.builder().id("A").billId("billId").uptTime(230 * TAG).DirectCurrent(30).DirectVoltage(380).DirectPower(10000).build(),
+                PileData.builder().id("A").billId("billId").uptTime(260 * TAG).DirectCurrent(30).DirectVoltage(380).DirectPower(10000).build(),
+                PileData.builder().id("A").billId("billId").uptTime(290 * TAG).DirectCurrent(30).DirectVoltage(380).DirectPower(10000).build(),
+                PileData.builder().id("A").billId("billId").uptTime(320 * TAG).DirectCurrent(30).DirectVoltage(380).DirectPower(10000).build(),
+                PileData.builder().id("A").billId("billId").uptTime(350 * TAG).DirectCurrent(30).DirectVoltage(380).DirectPower(10000).build(),
+                PileData.builder().id("A").billId("billId").uptTime(380 * TAG).DirectCurrent(30).DirectVoltage(380).DirectPower(10000).build()
+        );
 
+        for (PileData pileData : pileDataList){
+            ctx.collectWithTimestamp(pileData,pileData.getUptTime());
+        }
     }
 
     @Override
